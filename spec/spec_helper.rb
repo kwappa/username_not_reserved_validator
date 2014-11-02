865d53c9c3799b2d30cb6a718e468c8fb73092fd
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-11-02 15:36:21 +0900

    add spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,29 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'username_not_reserved_validator'
 
-class TestUser
+class TestModel
   include ActiveModel::Validations
 
-  validates :user, username_not_reserved: true
+  def initialize(attributes = {})
+    @attributes = attributes
+  end
+
+  def read_attribute_for_validation(key)
+    @attributes[key]
+  end
+end
+
+class WithoutValidationUser < TestModel
+end
+
+class WithValidationUser < TestModel
+  validates :name, username_not_reserved: true
+end
+
+class WithCaseInsencitiveValidationUser < TestModel
+  validates :name, username_not_reserved: { case_insencitive: false }
+end
+
+class WithAdditionalReservedNamesValidationUser < TestModel
+  validates :name, username_not_reserved: { additional_reserved_names: %w[additional_reserved_username] }
 end
--- spec/username_not_reserved_validator_spec.rb
+++ spec/username_not_reserved_validator_spec.rb
@@ -2,12 +2,141 @@ require 'spec_helper'
 
 describe UsernameNotReservedValidator do
 
-  let(:reserved_user_names) { UsernameNotReservedValidator::ReservedNames.list.sample(10) }
-  let(:valid_user_name) { 'valid_user_name' }
+  let(:reserved_usernames) { UsernameNotReservedValidator::ReservedNames.list }
+  let(:valid_username) { 'valid_username' }
+  let(:invalid_username) { reserved_usernames.first(30).sample }
+  let(:invalid_camelized_username) { reserved_usernames.first(30).sample.camelize }
+  let(:invalid_pluralized_username) { reserved_usernames.first(30).sample.pluralize }
+  let(:invalid_additional_username) { 'additional_reserved_username' }
 
-  describe 'reserved name list' do
-    it 'does not include sample user name' do
-      expect(reserved_user_names.map(&:downcase)).to_not include(valid_user_name)
+
+  describe 'sample user name' do
+    context 'when valid' do
+      it 'does not included in reserved list' do
+        expect(reserved_usernames.map(&:downcase)).to_not include(valid_username)
+        expect(reserved_usernames.map(&:downcase).map(&:pluralize)).to_not include(valid_username)
+      end
+    end
+
+    context 'when invalid' do
+      it 'included in reserved list' do
+        expect(reserved_usernames).to include(invalid_username)
+      end
+
+      context 'when camelized invalid name' do
+        it 'included only downcase list' do
+          expect(reserved_usernames).to_not include(invalid_camelized_username)
+          expect(reserved_usernames.map(&:downcase)).to include(invalid_camelized_username.downcase)
+        end
+      end
+
+      context 'when pluarized invalid name' do
+        it 'included in pluralized list' do
+          expect(reserved_usernames).to include(invalid_pluralized_username.singularize)
+        end
+      end
+
+      context 'when additional invalid name' do
+        it 'does not included in reserved list' do
+          expect(reserved_usernames.map(&:downcase)).to_not include(invalid_additional_username)
+          expect(reserved_usernames.map(&:downcase).map(&:pluralize)).to_not include(invalid_additional_username)
+        end
+      end
+    end
+  end
+
+  shared_examples_for 'accepts username' do
+    it { expect(user).to be_valid }
+  end
+
+  shared_examples_for 'rejects username' do
+    it { expect(user).to_not be_valid }
+  end
+
+
+  describe 'model without validation' do
+    subject(:user) { WithoutValidationUser.new(name: username) }
+
+    context 'with valid username' do
+      let(:username) { valid_username }
+      include_examples 'accepts username'
+    end
+
+    context 'with invalid username' do
+      let(:username) { invalid_username }
+      include_examples 'accepts username'
+    end
+
+        context 'with invalid camelized username' do
+      let(:username) { invalid_camelized_username }
+      include_examples 'accepts username'
+    end
+
+    context 'with invalid pluralized username' do
+      let(:username) { invalid_pluralized_username }
+      include_examples 'accepts username'
+    end
+
+    context 'with additionals invalid username' do
+      let(:username) { invalid_additional_username }
+      include_examples 'accepts username'
+    end
+  end
+
+  describe 'model with validation' do
+    subject(:user) { WithValidationUser.new(name: username) }
+
+    context 'with valid username' do
+      let(:username) { valid_username }
+      include_examples 'accepts username'
+    end
+
+    context 'with invalid username' do
+      let(:username) { invalid_username }
+      include_examples 'rejects username'
+    end
+
+    context 'with invalid camelized username' do
+      let(:username) { invalid_camelized_username }
+      include_examples 'rejects username'
+    end
+
+    context 'with invalid pluralized username' do
+      let(:username) { invalid_pluralized_username }
+      include_examples 'rejects username'
+    end
+  end
+
+  describe 'model with case insencitive validation' do
+    subject(:user) { WithCaseInsencitiveValidationUser.new(name: username) }
+
+    context 'with valid username' do
+      let(:username) { valid_username }
+      include_examples 'accepts username'
+    end
+
+    context 'with invalid username' do
+      let(:username) { invalid_username }
+      include_examples 'rejects username'
+    end
+
+    context 'with invalid camelized username' do
+      let(:username) { invalid_camelized_username }
+      include_examples 'accepts username'
+    end
+
+    context 'with invalid pluralized username' do
+      let(:username) { invalid_pluralized_username }
+      include_examples 'rejects username'
+    end
+  end
+
+  describe 'model with validation and additional reserved username' do
+    subject(:user) { WithAdditionalReservedNamesValidationUser.new(name: username) }
+
+    context 'with additionals invalid username' do
+      let(:username) { invalid_additional_username }
+      include_examples 'rejects username'
     end
   end
 end
